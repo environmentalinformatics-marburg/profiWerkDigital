2e8eaed4c69b845800aacbe06352547e73dff17e
Spaska Forteva <spaska.forteva@geo.uni-marburg.de>
2017-04-25 16:27:24 +0200

    Update

---
+++ pages/profiWerkDigital/subjects/anglistics/textarngmts/textarngmts_example.js
@@ -0,0 +1,6 @@
+function setColorRelations(){
+  document.getElementById("actColors").style.display = "block";
+  document.getElementById("example").style.display = "block";
+  document.getElementById("noColors").style.display = "none";
+ // document.getElementById("setColorRelations").style.display = "none";
+}
